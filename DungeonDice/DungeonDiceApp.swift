ec4b7cd72504b5bc3a03f47b3b627117cf4ae15e
Bob Witmer <bwitmer@golden.net>
2023-07-19 15:35:24 -0400

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Bob Witmer on 2023-07-19.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
