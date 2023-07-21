65415a81563be546d42e50681bdb828210da65d8
Bob Witmer <bwitmer@golden.net>
2023-07-21 14:58:08 -0400

    Chapter 4.5 Complete - Create an overflow to centre buttons that aren't if a full row Detect device rotation to recalculate the geometry sizing values

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -24,40 +24,75 @@ struct ContentView: View {
     }
     
     @State private var rollMessage = ""
+    @State private var overflowButtons = 0  // # of buttons in a less than full row
+    
+    let horizontalPadding: CGFloat = 16     // default padding size of '.padding()'
+    let spacing: CGFloat = 0                // between buttons
+    let buttonWidth: CGFloat = 102          // width of button
     
     var body: some View {
-        VStack {
-            
-            Text("Dungeon Dice")
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .foregroundColor(.red)
-            
-            Spacer()
-            
-            Text(rollMessage)
-                .font(.largeTitle)
-                .fontWeight(.medium)
-                .multilineTextAlignment(.center)
-                .frame(height: 150)
-            
-            Spacer()
-            
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 102))]) {
-                ForEach(Dice.allCases, id: \.self) { dice in
-                    Button("\(dice.rawValue)-sided") {
-                        rollMessage = "You rolled a \(dice.roll()) on a\n \(dice.rawValue)-sided dice"
+        GeometryReader { geo in
+            VStack {
+                
+                Text("Dungeon Dice")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(.red)
+                
+                Spacer()
+                
+                Text(rollMessage)
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+                    .multilineTextAlignment(.center)
+                    .frame(height: 150)
+                
+                Spacer()
+                
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: buttonWidth), spacing: spacing)]) {
+                    ForEach(Dice.allCases.dropLast(overflowButtons), id: \.self) { dice in
+                        Button("\(dice.rawValue)-sided") {
+                            rollMessage = "You rolled a \(dice.roll()) on a\n \(dice.rawValue)-sided dice"
+                        }
+                        .frame(width: buttonWidth)
                     }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.red)
                 }
-                .buttonStyle(.borderedProminent)
-                .tint(.red)
+
+                HStack {
+                    ForEach(Dice.allCases.suffix(overflowButtons), id: \.self) { dice in
+                        Button("\(dice.rawValue)-sided") {
+                            rollMessage = "You rolled a \(dice.roll()) on a\n \(dice.rawValue)-sided dice"
+                        }
+                        .frame(width: buttonWidth)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.red)
+                }
+                
+                
             }
-
-
-            
-            
+            .padding()
+            .onChange(of: geo.size.width, perform: { newValue in
+                arrangeGridItems(geo: geo)
+            })
+            .onAppear {
+                arrangeGridItems(geo: geo)
+            }
         }
-        .padding()
+
+    }
+    func arrangeGridItems(geo: GeometryProxy) {
+        var screenWidth = geo.size.width - (horizontalPadding * 2)  // padding on both sides
+        if Dice.allCases.count > 1 {
+            screenWidth += spacing
+        }
+        
+        // calculate the number of buttons in each row
+        let numberOfButtonsPerRow = Int(screenWidth) / Int(buttonWidth + spacing)
+        overflowButtons = Dice.allCases.count % numberOfButtonsPerRow
+
     }
 }
 
